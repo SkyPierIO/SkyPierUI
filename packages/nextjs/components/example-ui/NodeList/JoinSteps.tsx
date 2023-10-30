a9a69e2c9d61aaa73e9409c515efb3f40b686a15
smolgroot <nautilushell@protonmail.com>
2023-10-30 19:02:35 +0100

    Edit join steps with SOCKS details

--- packages/nextjs/components/example-ui/NodeList/JoinSteps.tsx
+++ packages/nextjs/components/example-ui/NodeList/JoinSteps.tsx
@@ -1,5 +1,7 @@
 import { forwardRef } from "react";
 import { Button } from "~~/components/ui/Button";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
+
 
 interface JoinStepsI {
   vpnData: {
@@ -12,13 +14,27 @@ export const JoinSteps = forwardRef((props: JoinStepsI, ref) => {
   return (
     <dialog ref={ref} className="modal">
       <div className="modal-box">
-        <h3 className="font-bold text-lg">Configure your browser network</h3>
-        <p>Configure the proxy in your browser network with the next information</p>
+        <h2 className="font-bold text-center text-xl text-success flex justify-center mt-5">
+          <CheckCircleIcon className="h-7 w-7 mr-3" />
+          Success. Libp2p forwarder enabled.
+        </h2>
+        <hr />
         <p>
-          <strong>Host:</strong> {props.vpnData?.ListeningAddress}
+          You can now forward your connection to the remote IPFS node.
         </p>
         <p>
-          <strong>Port:</strong> {props.vpnData?.ListeningPort}
+          A few configuration steps are necessary before browsing the web while hiding your IP!
+        </p>
+        <h3 className="font-bold text-lg">Configure your web browser</h3>
+        <p>In your browser Network configuration, define a new proxy with the next pieces of information:</p>
+        <p>
+          <strong>Proxy type:</strong> SOCKS v5
+        </p>
+        <p>
+          <strong>Host:</strong> <code>{props.vpnData?.ListeningAddress}</code>
+        </p>
+        <p>
+          <strong>Port:</strong> <code>{props.vpnData?.ListeningPort}</code>
         </p>
         <form method="dialog">
           <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
